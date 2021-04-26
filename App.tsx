b44f33ba03c4179c5c1456086242920745f543aa
Fabio Ferreira <fabio86ferreira@gmail>
2021-04-25 23:34:34 -0300

    new pages

--- App.tsx
+++ App.tsx
@@ -1,17 +1,22 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
-import { Welcome } from "./src/pages/Welcome";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold,
+} from "@expo-google-fonts/jost";
+
+import Routes from "./src/routes";
 
 const App = () => {
-  return <Welcome />;
-};
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+  });
 
-const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
+  if (!fontsLoaded) return <AppLoading />;
+
+  return <Routes />;
+};
 
 export default App;
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,28 +1,94 @@
 import React from "react";
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  Dimensions,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { useNavigation } from "@react-navigation/core";
+import { Entypo } from "@expo/vector-icons";
 
 import wateringImg from "../assets/watering.png";
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
 
 export const Welcome = () => {
+  const navigation = useNavigation();
+
+  const handleStart = () => {
+    navigation.navigate("UserIdentification");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Gerencie suas plantas de forma fácil</Text>
-      <Image source={wateringImg} />
-      <Text>
-        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
-        sempre que precisar.
-      </Text>
-      <TouchableOpacity>
-        <Text></Text>
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {"\n"}
+          suas plantas de{"\n"}
+          forma fácil
+        </Text>
+        <Image source={wateringImg} style={styles.image} resizeMode="contain" />
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas. {"\n"}
+          Nós cuidamos de lembrar você sempre que precisar.
+        </Text>
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.7}
+          onPress={handleStart}
+        >
+          <Text>
+            <Entypo name="chevron-right" style={styles.buttonIcon} />
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  wrapper: {
+    flex: 1,
     alignItems: "center",
-    justifyContent: "space-between",
+    justifyContent: "space-around",
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontFamily: fonts.heading,
+    fontSize: 28,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: colors.heading,
+    marginTop: 38,
+    lineHeight: 34,
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    textAlign: "center",
+    fontSize: 18,
+    paddingHorizontal: 20,
+    color: colors.heading,
+  },
+  image: {
+    height: Dimensions.get("window").width * 0.7,
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+  },
+  buttonIcon: {
+    fontSize: 32,
+    color: colors.white,
   },
 });
